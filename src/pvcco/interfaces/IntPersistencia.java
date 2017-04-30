ad874e4a6c4ecce66fd46f2707a7fffd2eab9987
zmsky <hiresky3201@gmail.com>
2017-04-29 21:22:23 -0700

    Cambios en lo mas bajo de las entidades. Codigo de barras se movio a Modelo en lugar de que cada talla tenga su codigo de barras individual.

--- src/pvcco/interfaces/IntPersistencia.java
+++ src/pvcco/interfaces/IntPersistencia.java
@@ -6,14 +6,29 @@ import objetosNegocio.Apartado;
 import objetosNegocio.BajaDeInventario;
 import objetosNegocio.Modelo;
 import objetosNegocio.MovimientoEnApartado;
+import objetosNegocio.MovimientoEnVenta;
 import objetosNegocio.Talla;
 import objetosNegocio.TallaApartado;
+import objetosNegocio.Venta;
+import objetosNegocio.VentaTalla;
 
 /**
  *
  * @author Raul Karim Sabag Ballesteros GAY
  */
 public interface IntPersistencia {
+    public void agregar(Venta venta) throws Exception;
+    public void actualizar(Venta venta) throws Exception;
+    public void eliminar(Venta venta) throws Exception;
+    
+    public void agregar(VentaTalla ventaTalla) throws Exception;
+    public void actualizar(VentaTalla ventaTalla) throws Exception;
+    public void eliminar(VentaTalla ventaTalla) throws Exception;
+    
+    public void agregar(MovimientoEnVenta mov) throws Exception;
+    public void actualizar(MovimientoEnVenta mov) throws Exception;
+    public void eliminar(MovimientoEnVenta mov) throws Exception;
+    
     public void agregar(TallaApartado talla) throws Exception;
     public void actualizar(TallaApartado talla) throws Exception;
     public void eliminar(TallaApartado talla) throws Exception;
@@ -39,10 +54,13 @@ public interface IntPersistencia {
     public void eliminar(BajaDeInventario baja) throws Exception;
     
     public Talla obten(Talla talla) throws Exception;
+    public Venta obten(Venta venta) throws Exception;
     public Modelo obten(Modelo modelo) throws Exception;
     public Apartado obten(Apartado apartado) throws Exception;
+    public VentaTalla obten(VentaTalla ventaTalla) throws Exception;
     public TallaApartado obten(TallaApartado talla) throws Exception;
     public BajaDeInventario obten(BajaDeInventario baja) throws Exception;
+    public MovimientoEnVenta obten(MovimientoEnVenta mov) throws Exception;
     public MovimientoEnApartado obten(MovimientoEnApartado mov) throws Exception;
     
     public List<TallaApartado> obtenTallasApartadas() throws Exception;
@@ -51,6 +69,9 @@ public interface IntPersistencia {
     public List<Modelo> obtenModelos() throws Exception;
     public List<Apartado> obtenApartados() throws Exception;
     public List<Talla> obtenTallas() throws Exception;
+    public List<Venta> obtenVentas() throws Exception;
+    public List<MovimientoEnVenta> obtenMovimientosEnVenta() throws Exception;
+    public List<VentaTalla> obtenVentaTallas() throws Exception;
     
     public Talla obtenTallaPorTalla(Talla talla) throws Exception;
     public Modelo obtenModeloPorNombre(Modelo modelo) throws Exception;
